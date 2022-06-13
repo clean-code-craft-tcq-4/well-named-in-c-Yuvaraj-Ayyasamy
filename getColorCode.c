d1ae01fe6f64c9169d7d838a724b37c057538a6a
Yuvaraj-Ayyasamy <105203963+Yuvaraj-Ayyasamy@users.noreply.github.com>
2022-06-13 14:55:53 +0530

    Update getColorCode.c

--- getColorCode.c
+++ getColorCode.c
@@ -1,14 +1,6 @@
 #include "colorCode_if.h"
 #include "getColorCode.h"
 
-const char* MajorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
-
-const char* MinorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
-
-int numberOfMajorColors = sizeof(MajorColor) / sizeof(MajorColor[0]);
-
-int numberOfMinorColors = sizeof(MinorColor) / sizeof(MinorColor[0]);
-
 void ColorPairToString(const ColorPair* colorPair, char* buffer) 
 {
     sprintf(buffer, "%s %s",MajorColor[colorPair->majorColor],minorColor[colorPair->minorColor]);
