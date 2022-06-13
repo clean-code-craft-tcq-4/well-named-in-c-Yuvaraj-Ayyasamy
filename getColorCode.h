b15ca4c1aa44a195740008c66a5bff425651b2d0
Yuvaraj-Ayyasamy <105203963+Yuvaraj-Ayyasamy@users.noreply.github.com>
2022-06-13 14:54:27 +0530

    Update getColorCode.h

--- getColorCode.h
+++ getColorCode.h
@@ -1,8 +1,8 @@
-const char* majorColor[];
+/* const char* majorColor[];
 const char* minorColor[];
 int numberOfMajorColors;
 int numberOfMinorColors;
 
 void ColorPairToString(const ColorPair* colorPair, char* buffer);
 ColorPair GetColorFromPairNumber(int pairNumber);
-int GetPairNumberFromColor(const ColorPair* colorPair);
+int GetPairNumberFromColor(const ColorPair* colorPair); */
