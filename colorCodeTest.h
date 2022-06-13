e980a3edc3dfc7a19490d36926e6c5437690dbf2
Yuvaraj-Ayyasamy <105203963+Yuvaraj-Ayyasamy@users.noreply.github.com>
2022-06-13 13:16:20 +0530

    Create colorCodeTest.h

---
+++ colorCodeTest.h
@@ -0,0 +1,2 @@
+extern void testNumberToPair(int pairNumber,enum MajorColor expectedMajor,enum MinorColor expectedMinor);
+extern void testPairToNumber(enum MajorColor major,enum MinorColor minor,int expectedPairNumber);
