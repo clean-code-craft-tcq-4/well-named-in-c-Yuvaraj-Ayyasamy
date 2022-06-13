66540021207fed952410e14dc40097474a19b75e
Yuvaraj-Ayyasamy <105203963+Yuvaraj-Ayyasamy@users.noreply.github.com>
2022-06-13 15:10:13 +0530

    Delete colorCodeTest.h

--- colorCodeTest.h
+++
@@ -1,2 +0,0 @@
-// void testNumberToPair(int pairNumber, enum MajorColor expectedMajor, enum MinorColor expectedMinor);
-// void testPairToNumber(enum MajorColor major, enum MinorColor minor, int expectedPairNumber);
