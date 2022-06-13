82f17e704a3e604fe9946aad49932fde93f9857c
Yuvaraj-Ayyasamy <105203963+Yuvaraj-Ayyasamy@users.noreply.github.com>
2022-06-13 14:55:45 +0530

    Update colorCode_if.h

--- colorCode_if.h
+++ colorCode_if.h
@@ -11,3 +11,16 @@ typedef struct {
     enum MajorColor majorColor;
     enum MinorColor minorColor;
 } ColorPair;
+
+const char* MajorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
+const char* MinorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+int numberOfMajorColors = sizeof(MajorColor) / sizeof(MajorColor[0]);
+int numberOfMinorColors = sizeof(MinorColor) / sizeof(MinorColor[0]);
+
+
+void ColorPairToString(const ColorPair* colorPair, char* buffer);
+ColorPair GetColorFromPairNumber(int pairNumber);
+int GetPairNumberFromColor(const ColorPair* colorPair);
+void testNumberToPair(int pairNumber, enum MajorColor expectedMajor, enum MinorColor expectedMinor);
+void testPairToNumber(enum MajorColor major, enum MinorColor minor, int expectedPairNumber);
+void printColorCodeReferenceManual(void);
