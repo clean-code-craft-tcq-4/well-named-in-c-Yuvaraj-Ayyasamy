7308babc3ac6d723958aeb3003bd3b8005da20d9
Yuvaraj-Ayyasamy <105203963+Yuvaraj-Ayyasamy@users.noreply.github.com>
2022-06-13 15:10:28 +0530

    Delete getColorCode.h

--- getColorCode.h
+++
@@ -1,8 +0,0 @@
-/* const char* majorColor[];
-const char* minorColor[];
-int numberOfMajorColors;
-int numberOfMinorColors;
-
-void ColorPairToString(const ColorPair* colorPair, char* buffer);
-ColorPair GetColorFromPairNumber(int pairNumber);
-int GetPairNumberFromColor(const ColorPair* colorPair); */
